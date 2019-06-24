326d301b334efd8554789ac433de9ed7d5e07d9b
Andrew Terry <andrewxtery@googlemail.com>
2019-06-24 18:31:43 -0500

    final commit

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,13 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,8 +25,9 @@ public class JobController {
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
+    Job jobID = jobData.findById(id);
+    model.addAttribute("jobs", jobID);
 
-        // TODO #1 - get the Job with the given ID and pass it into the view
 
         return "job-detail";
     }
@@ -37,11 +41,22 @@ public class JobController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model, @Valid JobForm jobForm, Errors errors) {
 
+        if(errors.hasErrors()) {
+
+            return"new-job";
+        }
+        Employer anEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location aLocation = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType aPosition = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency aSkill = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+
+        Job newJob = new Job(jobForm.getName(), anEmployer, aLocation, aPosition, aSkill);
+        jobData.add(newJob);
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        return"redirect:?id=" + newJob.getId();
 
     }
 }
--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -1,10 +1,8 @@
 package org.launchcode.models.forms;
 
-import org.launchcode.models.CoreCompetency;
-import org.launchcode.models.Employer;
-import org.launchcode.models.Location;
-import org.launchcode.models.PositionType;
+import org.launchcode.models.*;
 import org.launchcode.models.data.JobData;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -19,8 +17,16 @@ public class JobForm {
     @Size(min=1, message = "Name may not be empty")
     private String name;
 
+
     @NotNull
     private int employerId;
+
+
+    private int locationId;
+    private int coreCompetencyId;
+    private int positionTypeId;
+
+
 
     /*
         TODO #3 - Included other fields needed to create a job,
@@ -42,6 +48,9 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
     }
 
@@ -91,5 +100,29 @@ public class JobForm {
 
     public void setPositionTypes(ArrayList<PositionType> positionTypes) {
         this.positionTypes = positionTypes;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
     }
 }
